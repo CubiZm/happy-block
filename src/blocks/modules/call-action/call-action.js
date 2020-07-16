a4e5acb6462624d10b2de27fd8c93b30eb6ee7ae
CubiZm <karelense@gmail.com>
2020-07-17 00:21:40 +0300

    start new project, added page, blocks and other

---
+++ src/blocks/modules/call-action/call-action.js
@@ -0,0 +1,13 @@
+modules.define('call-action', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
